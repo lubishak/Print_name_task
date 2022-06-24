a500e45842c76667879d8d855747d7df3dac7a41
Ishaku Ayuba <lubishak@gmail.com>
2022-06-24 19:06:09 +0100

    first commit

---
+++ printName.js
@@ -0,0 +1,3 @@
+document.getElementById('name').innerHTML = 'Name: Ishaku Ayuba';
+document.getElementById('heigth').innerHTML = 'Heigth: 1.9';
+document.getElementById('country').innerHTML = 'Country: Nigeria';
